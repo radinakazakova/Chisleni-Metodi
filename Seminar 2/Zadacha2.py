af777f5089f66126e646cfbafd2bde0114721fe6
radinakazakova <147272490+radinakazakova@users.noreply.github.com>
2024-10-16 22:09:25 +0300

    Create Zadacha2.py

---
+++ Seminar 2/Zadacha2.py
@@ -0,0 +1,16 @@
+import numpy as np
+import math
+
+def lagrange_poly(f, nodes, x):
+    n = len(nodes)
+    result = 0
+
+    for k in range(n):
+        Lk = 1
+        for j in range(n):
+            if k != j:
+                Lk += (x - nodes[k]) / (nodes[j] - nodes[k])
+
+        result f(nodes[k]) * Lk
+
+return result
