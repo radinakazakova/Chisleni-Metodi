7ee28b5ddf3684b51b114d2de8183194e35466c6
radinakazakova <147272490+radinakazakova@users.noreply.github.com>
2024-11-24 23:12:17 +0200

    Create Zad3.py

---
+++ Seminar6/Zad3.py
@@ -0,0 +1,48 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import math 
+import sympy as sp
+
+x_values = [1, 1.5, 3, 4, 6]
+
+def createTrigMatrix(values):
+    size = len(values)
+    matrix = []
+    for i in range(size):
+        row = []
+        counter = 1
+        for j in range(size):
+            if j == 0:
+                row.append(1)
+            elif j% 2 != 0:
+                row.append( np.cos(counter * values[i]) )
+            else:
+                row.append( np.sin(counter * values[i]) )
+                counter += 1
+        matrix.append(row)
+    return np.array(matrix)
+            
+A = createTrigMatrix(x_values)
+y_values = np.array([0, 1, 1.2, 4, 2])
+coef = np.array(np.linalg.solve(A, y_values))
+x = sp.symbols('x')
+
+def createTrigPolynom(coeficient):
+     size = len(coeficient)
+     expr = 0
+     counter = 1
+     for i in range(size):
+         if i == 0:
+             expr += coeficient[i]
+         elif i % 2 != 0: 
+             expr += coeficient[i] * sp.cos(counter * x)
+         else:
+             expr += coeficient[i] * sp.sin(counter * x)
+             counter += 1
+     return expr
+
+polynom = createTrigPolynom(coef)
+f = sp.lambdify(x, polynom)
+f(1) #okolo 0
+f(4) #smqta pravilno
+f(6)
