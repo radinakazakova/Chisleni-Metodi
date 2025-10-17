323391bbbe0c33fcc875ec03f570dc5094342d95
Radina Kazakova <147272490+radinakazakova@users.noreply.github.com>
2025-10-17 20:42:14 +0300

    Update Zad6.py

--- Seminar4/Zad6.py
+++ Seminar4/Zad6.py
@@ -1,3 +1,4 @@
+# Initial try for solution (incorrect)
 import numpy as np
 import math
 import sympy
@@ -32,3 +33,24 @@ print(polynom(30))
 x = np.linspace(0, 77, 1000)
 plt.plot(x, polynom(x))
 plt.show()
+
+# The correct solution
+# SymPy is a Python library for symbolic computations
+from sympy import Symbol, expand, Eq, solve
+
+def newton_poly(nodes, values, x):
+    poly = 0
+    product = 1
+    for i in range(nodes.size):
+        poly += divided_difference(nodes[ : i + 1],values[ : i + 1]) * product
+        product *= (x - nodes[i])
+    return poly
+
+time = np.array([0.163928, 0.53282, 3.00007, 11.2078, 26.7487, 47.3297, 76.8061])
+elements = np.array([10, 20, 50, 100, 150, 200, 250])
+x = Symbol('x')
+left_part = expand(newton_poly(elements, time, x)) 
+
+equation = Eq(left_part, 30) #30 da e reshenie na uravnenieto - demek to e vremeto i tursim x
+sol = solve(equation, x)
+print(sol)
