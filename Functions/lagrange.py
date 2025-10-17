75bd2cd834529a556feaf112a7c15e73b1162724
Radina Kazakova <147272490+radinakazakova@users.noreply.github.com>
2025-10-17 22:00:49 +0300

    Create lagrange.py

---
+++ Functions/lagrange.py
@@ -0,0 +1,32 @@
+import numpy as np
+import math
+
+def lagrange_poly(f, nodes, x):
+    n = len(nodes)
+    result = 0
+
+    for k in range(n):
+        Lk = 1
+        for j in range(n): #namirame bazovite polinomi
+            if k != j:
+                Lk *= (x - nodes[j]) / (nodes[k] - nodes[j])
+
+        result += f(nodes[k]) * Lk
+
+    return result
+
+#or
+
+def lagrange_poly(nodes, values):
+    n = len(nodes)
+    result = 0
+
+    for k in range(n):
+        Lk = 1
+        for j in range(n): #namirame bazovite polinomi
+            if k != j:
+                Lk *= (x - nodes[j]) / (nodes[k] - nodes[j])
+
+        result += values[k] * Lk
+
+    return result
