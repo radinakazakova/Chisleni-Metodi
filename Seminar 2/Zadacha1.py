c888e80160539de076a57383ac4947c9091b39ed
Radina Kazakova <147272490+radinakazakova@users.noreply.github.com>
2025-10-17 20:35:09 +0300

    Update Zadacha1.py

--- Seminar 2/Zadacha1.py
+++ Seminar 2/Zadacha1.py
@@ -1,37 +1,65 @@
 import numpy as np
 import math
-
-x0 = 0
-x1 = math.pi / 6
-x2 = math.pi / 3
-x3 = math.pi / 2
-
-y0 = math.sin(x0)
-y1 = math.sin(x1)
-y2 = math.sin(x2)
-y3 = math.sin(x3)
-
-def l03(x):
-    return ((x - x1)*(x - x2)*(x - x3)) / ((x0 - x1)*(x0 - x2)*(x0 - x3))
-
-def l13(x):
-    return ((x - x0)*(x - x2)*(x - x3)) / ((x1 - x0)*(x1 - x2)*(x1 - x3))
-
-def l23(x):
-    return ((x - x0)*(x - x1)*(x - x3)) / ((x2 - x0)*(x2 - x1)*(x2 - x3))
-
-def l33(x):
-    return ((x - x0)*(x - x1)*(x - x2)) / ((x3 - x0)*(x3 - x1)*(x3 - x2))
-
-def p(x):
-    return y0*l03(x) + y1*l13(x) + y2 * l23(x) + y3 * l33(x)
-
-result = p(math.pi / 5)
-print(result)
-
 import matplotlib.pyplot as plt
 
-x_axis = np.linspace(0, math.pi / 2, 100)
-#x_axis = np.array([x0, x1, x2, x3])
-plt.plot(x_axis, np.sin(x_axis), x_axis, p(x_axis))
+#construct the interpoalting polynomial
+x0 = 0;
+x1 = math.pi/6;
+x2 = math.pi/3;
+x3 = math.pi/2;
+
+def f(x):
+    return np.sin(x)
+
+y0 = f(x0);
+y1 = f(x1);
+y2 = f(x2);
+y3 = f(x3);
+
+def l0(x):
+    return (x - x1)*(x - x2)*(x - x3)/((x0 - x1)*(x0 - x2)*(x0 - x3))
+def l1(x):
+    return (x - x0)*(x - x2)*(x - x3)/((x1 - x0)*(x1 - x2)*(x1 - x3))
+def l2(x):
+    return (x - x0)*(x - x1)*(x - x3)/((x2 - x0)*(x2 - x1)*(x2 - x3))
+def l3(x):
+    return (x - x0)*(x - x1)*(x - x2)/((x3 - x0)*(x3 - x1)*(x3 - x2))
+
+def L3(x):
+    return y0 * l0(x) + y1 * l1(x) + y2 * l2(x) + y3 * l3(x)
+
+#compute the approximation 
+exact_value = math.sin(math.pi/5)
+approx_value = L3(math.pi/5)
+error_estimate = 1/24 * abs(math.pi/5 * (math.pi/5 - math.pi/6) * (math.pi/5 - math.pi/3) * (math.pi/5 - math.pi/2))
+print("Exact value: ", exact_value)
+print("Approx value: ", approx_value)
+print("Absolute error: ", abs(exact_value - approx_value))
+print("Error estimate: ", error_estimate)
+
+#plot data
+nodes = np.array([0, math.pi/6, math.pi/3, math.pi/2])
+f_values = np.sin(nodes)
+plt.scatter(nodes, f_values)
+x_axis = np.linspace(0, math.pi/2, 1000)
+plt.plot(x_axis, np.sin(x_axis), color='y')
+plt.plot(x_axis, L3(x_axis), color='black', linestyle='dashed')
+plt.xlabel('x')
+plt.legend(['data','sin(x)',' L3(f;x)'])
+plt.show()
+
+
+#plot errors as functions of  x
+def abs_error_func(x):
+    return abs(f(x) - L3(x))
+
+def error_estimate(x):
+    return 1/24 * abs(x * (x - math.pi/6) * (x - math.pi/3) * (x - math.pi/2))
+
+plt.plot(x_axis, abs_error_func(x_axis))
+plt.legend(['Absolute error'])
+plt.show()
+plt.plot(x_axis, abs_error_func(x_axis))
+plt.plot(x_axis, error_estimate(x_axis))
+plt.legend(["Absolute error","Error estimate"])
 plt.show()
